541d6a81c85adadfb5febf462190766f19b06345
pabloCC <chaussepablo@gmail.com>
2025-07-23 10:29:51 -0400

    add go basics if/else, swtich, loops

--- 01-basics/controlflow.go
+++ 01-basics/controlflow.go
@@ -1,13 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"time"
+)
 
 func controlFlow() {
-	even := 2
-	odd := 3
+	defer fmt.Println("I guess we're all done here?")
+	firstValue := 2
+	secondValue := 3
 
-	fmt.Printf("is %d even? %t\n", even, isEven(even))
-	fmt.Printf("is %d even? %t\n", odd, isEven(odd))
+	fmt.Printf("is %d even? %t\n", firstValue, isEven(firstValue))
+	fmt.Printf("is %d even? %t\n", secondValue, isEven(secondValue))
+
+	// If/Else
+	if isEven(firstValue + secondValue) {
+		fmt.Println("both even or both odd")
+	} else {
+		fmt.Println("one even and one odd")
+	}
+
+	// Switch
+	day := time.Now().Weekday().String()
+	switch day {
+	case "Monday":
+		fmt.Println("I hate mondays")
+	case "Sunday":
+		fmt.Println("I love lasagna")
+	default:
+		fmt.Printf("I cant believe its %s already!\n", day)
+	}
+
+	//For loop
+	for i := 1; i <= 5; i++ {
+		fmt.Println(strings.Repeat("i", i))
+	}
+	var age = 0
+	for {
+		if age > 2 {
+			fmt.Println("Hello mother")
+			break
+		}
+		fmt.Println("Googoo gaga")
+		age++
+	}
 }
 
 func isEven(n int) bool {
